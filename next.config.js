bf9ebd1a2a66304d4f2faa3dd627d825988a59b5
NishchayVashishtha <164996852+NishchayVashishtha@users.noreply.github.com>
2026-01-30 22:59:46 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  transpilePackages: ['three', '@react-three/fiber', '@react-three/drei'],
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'three': require.resolve('three')
+    }
+    return config
+  }
+}
+
+module.exports = nextConfig
